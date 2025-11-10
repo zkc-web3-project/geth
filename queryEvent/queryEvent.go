b7196cf4461edb0f80b93136a73e8d47e7547f49
zkc <zhangkaocheng@allcam.com.cn>
2025-11-10 16:21:55 +0800

    监听并解析合约事件

---
+++ queryEvent/queryEvent.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var StoreABI = `[{"inputs":[{"internalType":"string","name":"_version","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"bytes32","name":"key","type":"bytes32"},{"indexed":false,"internalType":"bytes32","name":"value","type":"bytes32"}],"name":"ItemSet","type":"event"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"items","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"key","type":"bytes32"},{"internalType":"bytes32","name":"value","type":"bytes32"}],"name":"setItem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]`
+
+const (
+	contractAddr = "0x1Fc54de16F09Da082f3b546323036Ae86a35F956"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/aab47bf44d3b4988916ae6383cb5d490")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contractAddress := common.HexToAddress(contractAddr)
+	query := ethereum.FilterQuery{
+		FromBlock: big.NewInt(6920583), //起始区块高度
+		// ToBlock:   big.NewInt(2394201), //结束区块高度
+		Addresses: []common.Address{
+			contractAddress, //只查询此合约下的事件
+		},
+
+		//只查询此合约下的特定类型的事件(当前查询ItemSet事件)
+		// Topics: [][]common.Hash{{
+		// 	crypto.Keccak256Hash([]byte("ItemSet(bytes32,bytes32)")),
+		// }},
+	}
+
+	//从客户端获取日志
+	logs, err := client.FilterLogs(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//解析合约abi定义，为后续事件解码做准备
+	contractAbi, err := abi.JSON(strings.NewReader(StoreABI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, vLog := range logs {
+		fmt.Printf("区块hash:%s\n", vLog.BlockHash.Hex())
+		fmt.Printf("区块号:%d\n", vLog.BlockNumber)
+		fmt.Printf("交易hash:%s\n", vLog.TxHash.Hex())
+		event := struct {
+			Key   [32]byte
+			Value [32]byte
+		}{}
+		//解码事件数据，"ItemSet"是合约abi定义的事件名称
+		err := contractAbi.UnpackIntoInterface(&event, "ItemSet", vLog.Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("解码后的事件数据的Key:%s\n", common.Bytes2Hex(event.Key[:]))
+		fmt.Printf("解码后事件数据的Value:%s\n", common.Bytes2Hex(event.Value[:]))
+		//处理事件的topics
+		var topics []string
+		for i := range vLog.Topics {
+			topics = append(topics, vLog.Topics[i].Hex())
+		}
+
+		fmt.Println("topics[0]=", topics[0])
+		if len(topics) > 1 {
+			fmt.Println("indexed topics:", topics[1:])
+		}
+	}
+
+	eventSignature := []byte("ItemSet(bytes32,bytes32)")
+	hash := crypto.Keccak256Hash(eventSignature)
+	fmt.Println("signature topics=", hash.Hex())
+}
